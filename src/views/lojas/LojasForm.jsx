4b47a6694d1f558c2b0f341db8b6392ee49d594b
marcelo <marcelo@dotse.com.br>
2021-11-22 12:20:50 -0300

    fix: tag editar

--- src/views/lojas/LojasForm.jsx
+++ src/views/lojas/LojasForm.jsx
@@ -11,27 +11,31 @@ import React, { useEffect } from "react";
 import { useHistory, useParams } from "react-router";
 import UserService from "src/services/auth/user.service";
 import { useForm, Controller } from "react-hook-form";
-import useYupValidationResolver from 'src/hooks/useYupValidationResolver';
+import useYupValidationResolver from "src/hooks/useYupValidationResolver";
 import * as yup from "yup";
 const initialValues = {
   nome: "",
   descricao: "",
-
 };
-const validationSchema = yup.object({
-nome: yup.string().required("O campo nome é obrigatório"),
-descricao: yup.string().required("O campo descrição é obrigatório"),
-}).required();
-
+const validationSchema = yup
+  .object({
+    nome: yup.string().required("O campo nome é obrigatório"),
+    descricao: yup.string().required("O campo descrição é obrigatório"),
+  })
+  .required();
 
 export const LojasForm = (item = null) => {
   const resolver = useYupValidationResolver(validationSchema);
-  const { control, handleSubmit,  reset, formState:{ errors } } = useForm({mode: 'onBlur', resolver});
+  const {
+    control,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm({ mode: "onBlur", resolver });
   const history = useHistory();
   let { id } = useParams();
   const [data, setData] = React.useState(initialValues);
   const [load, setLoad] = React.useState(false);
-
 
   useEffect(() => {
     if (id) {
@@ -46,23 +50,23 @@ export const LojasForm = (item = null) => {
     }
   }, [id]);
 
-useEffect(() => {
-  reset(data);
-}, [data, reset]);
+  useEffect(() => {
+    reset(data);
+  }, [data, reset]);
+
   const onSubmit = (e) => {
-    console.log(e)
+    console.log(e);
     // e.preventDefault();
     if (id) {
       UserService.update(`lojas/${id}`, e).then((response) => {
         console.log(response);
       });
     } else {
-      UserService.create('lojas', e).then((response) => {
+      UserService.create("lojas", e).then((response) => {
         console.log(response);
       });
-
     }
-    history.push(`/lojas`)
+    history.push(`/lojas`);
   };
 
   // const onchange = (e) => {
@@ -73,7 +77,6 @@ useEffect(() => {
   if (load) {
     return <div>Carregando...</div>;
   }
-
 
   return (
     <>
@@ -86,32 +89,22 @@ useEffect(() => {
                 name="nome"
                 control={control}
                 defaultValue={data.nome ?? ""}
-                render={({ field }, ref) => (
-                    <CInput
-                    {...field}
-                    ref={ref}
-                 />
-
-                 )}
-                 />
-                 <p>{errors.nome?.message}</p>
+                render={({ field }, ref) => <CInput {...field} ref={ref} />}
+              />
+              <p>{errors.nome?.message}</p>
             </CFormGroup>
           </CCol>
           <CCol xs="12" md="6">
             <CFormGroup>
-            <CLabel htmlFor="descricao">Descrição</CLabel>
-            <Controller
-              name="descricao"
-              control={control}
-              defaultValue={data.descricao ?? ""}
-              render={({ field }, ref) => (
-                  <CInput
-                  {...field}
-                  ref={ref}/>
-                  )}
-                  />
-                  <p>{errors.descricao?.message}</p>
-                  </CFormGroup>
+              <CLabel htmlFor="descricao">Descrição</CLabel>
+              <Controller
+                name="descricao"
+                control={control}
+                defaultValue={data.descricao ?? ""}
+                render={({ field }, ref) => <CInput {...field} ref={ref} />}
+              />
+              <p>{errors.descricao?.message}</p>
+            </CFormGroup>
           </CCol>
         </CRow>
         <CButton type="submit" color="info">
--- src/views/lojas/new_loja.jsx
+++ src/views/lojas/new_loja.jsx
@@ -1,13 +1,15 @@
 import CIcon from "@coreui/icons-react";
 import { CCard, CCardHeader, CBadge, CCardBody } from "@coreui/react";
+import { useParams } from "react-router";
 import { LojasForm } from "./LojasForm";
 // import {VendedorForm} from "./components/VendedorForm"
 function NewLoja  ()  {
+  let { id } = useParams();
   return (
     <><CCard>
       <CCardHeader>
         <CIcon name="cil-people" />
-        <CBadge color="success">Cadastrar nova loja</CBadge>
+        <CBadge color="success">{`${id ? 'Editar': 'Cadastrar nova'}  loja`}</CBadge>
       </CCardHeader>
       <CCardBody>
         <LojasForm/>
